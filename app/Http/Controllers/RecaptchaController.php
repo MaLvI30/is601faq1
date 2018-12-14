8c9834295bf9b6ce83f3351f31d6a963f1cbb46f
malvi <mrp92@njit.edu>
2018-12-14 14:12:28 -0500

    Feature: Created recaptcha controller and added secret key from g-recaptcha.

---
+++ app/Http/Controllers/RecaptchaController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RecaptchaController extends Controller
+{
+ public function displayForm( Request $request){
+     
+     $token = $request->input('g-recaptcha-response');
+
+     if ($token) {
+         $client = new Client();
+         $response = $client->post('https://www.google.com/recaptcha/api/siteverify', [
+             'form_params' => array(
+                 'secret' => '',
+                 'response' => $token
+             )
+         ]);
+         $results = json_decode($response->getBody()->getContents());
+     }
+     else {
+         return redirect('/');
+
+     }
+
+ }
+}
